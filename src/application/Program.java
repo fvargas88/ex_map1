c6717e1a5b8edafa30847521d88909c97cbea30c
fvargas88 <fvargas_88@hotmail.com>
2023-03-09 08:06:48 -0300

    Exemplo de Map usando o TreeMap, mais lento e ordenado

---
+++ src/application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Map<String, String> cookies = new TreeMap<>();
+		
+		cookies.put("usarname", "Maria");
+		cookies.put("email", "maria@gmail.com");
+		cookies.put("phone", "99087898");
+		
+		cookies.remove("email");
+		
+		System.out.println("contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
+		
+		
+		System.out.println("ALL COOKIES");
+		for(String  key: cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
+		}
+
+	}
+
+}
+
+//TreeMap ele ordena, como o exemplo é com string ele vai ordenar em ordem alfabética
+//obs: o método não aceita repetição da chave, caso haja ele vai sobrescrever a chave
